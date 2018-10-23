152dedbb97f9e14c85830cb379c9bbffdc333fdd
Jensen Jiang <jensenjiang1995@gmail.com>
2018-10-22 21:42:40 -0400

    lc 319

---
+++ LeetCode/319.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: Jensen Jiang<jensenjiang1995@gmail.com>
+ * Problem ID: 319
+ * Problem Title: Bulb Switcher
+ * Keyword: math
+ *
+ */
+
+#include <cmath>
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
